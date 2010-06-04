8982b01746d0d96baae69018781994e5f74edca6
Benjamin Saller <bcsaller@gmail.com>
2010-06-04 14:37:35 -0700

    increment version for push

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools.extension import Extension
 
 setup(
     name="rbtree",
-    version="0.8.8",
+    version="0.8.9",
     packages=find_packages('src', exclude=["*.tests", "ez_setup.py"]),
     package_dir={'': 'src'},
     package_data={'': ['*.txt'], },
