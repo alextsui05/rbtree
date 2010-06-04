54e9c8782a197c6223bfc953527258641f733f10
Benjamin Saller <bcsaller@gmail.com>
2010-06-04 14:34:58 -0700

    apparently sdist was broken. repair MANIFEST to include ez_setup properly

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
-import ez_setup
-ez_setup.use_setuptools()
+import sys
+try:
+    from ez_setup import use_setuptools
+    use_setuptools()
+except ImportError:
+    pass
+
 
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
@@ -7,13 +12,13 @@ from setuptools.extension import Extension
 setup(
     name="rbtree",
     version="0.8.8",
-    packages=find_packages('src', exclude=["*.tests"]),
+    packages=find_packages('src', exclude=["*.tests", "ez_setup.py"]),
     package_dir={'': 'src'},
     package_data={'': ['*.txt'], },
     ext_modules=[Extension("rbtree", ["src/rbtree_impl.c",
                                       "src/rbtree.pyx"],
-                            libraries=[],
-                            include_dirs=['./src', ])
+                           libraries=[],
+                           include_dirs=['./src', ])
                   ],
     test_suite="tests.test_rbtree",
     zip_safe=False,
@@ -23,6 +28,7 @@ setup(
     description="""A red black tree with extended iterator
     support.""",
     download_url="http://bitbucket.org/bcsaller/rbtree/",
+    url="http://bitbucket.org/bcsaller/rbtree/",
     license='GPL 3',
     keywords="rbtree red-black tree data-structure",
     )
