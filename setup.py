6f0eed04065ffd813f38939253a107e94ded34c6
Alex Tsui <alextsui05@gmail.com>
2016-09-18 21:16:41 -0700

    Update setup.py to use cython

--- setup.py
+++ setup.py
@@ -8,18 +8,21 @@ except ImportError:
 
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
+from Cython.Build import cythonize
 
+extensions = [
+        Extension("rbtree", ["src/rbtree_impl.c",
+            "src/rbtree.pyx"],
+            libraries=[],
+            include_dirs=['./src', ])
+]
 setup(
     name="rbtree",
-    version="0.9.0",
+    version="0.9.1",
     packages=find_packages('src', exclude=["*.tests", "ez_setup.py"]),
     package_dir={'': 'src'},
     package_data={'': ['*.txt'], },
-    ext_modules=[Extension("rbtree", ["src/rbtree_impl.c",
-                                      "src/rbtree.pyx"],
-                           libraries=[],
-                           include_dirs=['./src', ])
-                  ],
+    ext_modules=cythonize(extensions),
     test_suite="tests.test_rbtree",
     zip_safe=False,
     include_package_data=True,
@@ -32,4 +35,28 @@ setup(
     license='GPL 3',
     keywords="rbtree red-black tree data-structure",
     )
+
+#setup(
+#    name="rbtree",
+#    version="0.9.0",
+#    packages=find_packages('src', exclude=["*.tests", "ez_setup.py"]),
+#    package_dir={'': 'src'},
+#    package_data={'': ['*.txt'], },
+#    ext_modules=[Extension("rbtree", ["src/rbtree_impl.c",
+#                                      "src/rbtree.pyx"],
+#                           libraries=[],
+#                           include_dirs=['./src', ])
+#                  ],
+#    test_suite="tests.test_rbtree",
+#    zip_safe=False,
+#    include_package_data=True,
+#    author='Benjamin Saller',
+#    author_email='bcsaller@gmail.com',
+#    description="""A red black tree with extended iterator
+#    support.""",
+#    download_url="http://bitbucket.org/bcsaller/rbtree/",
+#    url="http://bitbucket.org/bcsaller/rbtree/",
+#    license='GPL 3',
+#    keywords="rbtree red-black tree data-structure",
+#    )
 
